8b87509696f94b767c5610cdfc6f8e4db8aba5b4
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2021-04-26 13:41:30 +0200

    moved snake and food into a entity file

--- gymSnake/SnakeComponents.py
+++ gymSnake/SnakeComponents.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 import random
+import Entity
+import Config
 
 SCREEN_WIDTH = 480
 SCREEN_HEIGHT = 480
@@ -15,56 +17,28 @@ LEFT = (-1, 0)
 RIGHT = (1, 0)
 
 
-class Food(object):
-    def __init__(self, grid_width, grid_height, grid_size):
-        self.grid_width = grid_width
-        self.grid_height = grid_height
-        self.grid_size = grid_size
-        self.position = (0, 0)  # todo make default a random position instead of 0,0
-        self.color = (223, 163, 49)
-        self.randomize_position()
+class Directions(object):
+    def __init__(self):
+        self.up = (0, -1)
+        self.down = (0, 1)
+        self.left = (-1, 0)
+        self.right = (1, 0)
 
-    def randomize_position(self):
-        self.position = (
-            random.randint(0, int(self.grid_width - 1)) * self.grid_size[0],
-            random.randint(0, int(self.grid_height - 1)) * self.grid_size[1]
-        )
+    def random(self):
+        return random.choice([self.up, self.down, self.left, self.right])
 
 
-class Snake(object):
-    def __init__(self, start_position):
-        self.length = 1
-        self.positions = start_position
-        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
-        self.color = (17, 24, 47)
-        self.score = 0
-        self.time = 0
-        self.self_bite = False
-
-    def get_head_position(self):
-        return self.positions[0]
-
-    def turn(self, point):
-        if self.length > 1 and (point[0] * -1, point[1] * -1) == self.direction:
-            return
-        else:
-            self.direction = point
-
-    def move(self):
-        cur = self.get_head_position()
-        x, y = self.direction
-        new = (((cur[0] + (x * GRID_SIZE[0])) % SCREEN_WIDTH), (cur[1] + (y * GRID_SIZE[1])) % SCREEN_HEIGHT)
-        if len(self.positions) > 2 and new in self.positions[2:]:
-            self.self_bite = True
-        else:
-            self.positions.insert(0, new)
-            if len(self.positions) > self.length:
-                self.positions.pop()
+class Grid(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.size = (width, height)
 
 
 class Draw(object):
-    def __init__(self, screen, grid_width, grid_height, grid_size):
-        self.screen = screen
+    def __init__(self, screen_size, grid_width, grid_height, grid_size):
+        self.screen_size = screen_size
+        self.screen = pygame.display.set_mode(screen_size, 0, 32)
         self.grid_width = grid_width
         self.grid_height = grid_height
         self.grid_size = grid_size
@@ -80,14 +54,18 @@ class Draw(object):
                 pygame.draw.rect(self.surface, color, r)
 
     def draw_food(self, food):
-        r = pygame.Rect(food.position, self.grid_size)
+        r = pygame.Rect(self.cal_position(food.position), self.grid_size)
         pygame.draw.rect(self.surface, food.color, r)
 
     def draw_snake(self, snake):
         for p in snake.positions:
-            r = pygame.Rect((p[0], p[1]), self.grid_size)
+            r = pygame.Rect(self.cal_position((p[0], p[1])), self.grid_size)
             pygame.draw.rect(self.surface, snake.color, r)
             pygame.draw.rect(self.surface, (93, 216, 228), r, 1)
+
+    def cal_position(self, position):
+        x, y = position
+        return x * self.grid_size[0], y * self.grid_size[1]
 
     def draw_score(self, score):
         score_text = self.font.render("Score {0}".format(score), True, (255, 0, 0))
@@ -107,17 +85,22 @@ class Draw(object):
 
 
 class Game(object):
-    def __init__(self, mode, draw):
+    def __init__(self, mode):
+        self.directions = Directions()
+        self.grid = Grid(GRID_WIDTH, GRID_HEIGHT)
         self.mode = mode
-        self.draw = draw
-        self.snake = Snake([((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))])
-        self.food = Food(GRID_WIDTH, GRID_HEIGHT, GRID_SIZE)
+        self.start_pos = [((self.grid.width / 2), (self.grid.height / 2))]
+        self.snake = Entity.Snake(self.grid)
+        self.food = Entity.Food(self.grid)
         self.score = 0
         self.clock = pygame.time.Clock()
 
-    def step(self):
+    def step(self, action):
         self.clock.tick(10)
-        self.handle_keys()
+        if self.mode is "human":
+            self.control_keys()
+        else:
+            self.snake.turn(action)
         self.snake.move()
         if self.snake.self_bite:
             self.reset()
@@ -130,20 +113,25 @@ class Game(object):
         self.render(self.mode)
 
     def reset(self):
-        self.snake = Snake([((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))])
+        self.snake = Entity.Snake(self.grid)
+        self.food = Entity.Food(self.grid, (255, 0, 0))
         self.score = 0
 
     def render(self, mode='human'):
+        draw = None
         if mode == "human":
-            self.draw.step(self.snake, self.food, self.score)
+            if draw is None:
+                draw = Draw((SCREEN_WIDTH, SCREEN_HEIGHT), GRID_WIDTH, GRID_HEIGHT, GRID_SIZE)
+            draw.step(self.snake, self.food, self.score)
             pygame.display.update()
 
     def print_positions(self):
         snake_position = self.snake.positions[0]
         print("Snake position is x:{0} and y: {1}".format(snake_position[0], snake_position[1]))
         print("Food position is x:{0} and y: {1}".format(self.food.position[0], self.food.position[1]))
+        print("Score is: {0}".format(self.score))
 
-    def handle_keys(self):
+    def control_keys(self):
         movement_keys = {
             pygame.K_UP: UP,
             pygame.K_DOWN: DOWN,
@@ -162,6 +150,8 @@ class Game(object):
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
+                elif event.key == pygame.K_r:
+                    self.reset()
                 else:
                     try:
                         self.snake.turn(movement_keys[event.key])
@@ -171,13 +161,12 @@ class Game(object):
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
-    render = Draw(screen, GRID_WIDTH, GRID_HEIGHT, GRID_SIZE)
-    game = Game("human", render)
+    game = Game("human")
 
     is_running = True
     while is_running:
-        game.step()
+        action = random.choice([UP, DOWN, LEFT, RIGHT])
+        game.step(action)
 
 
 main()
