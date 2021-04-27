3df24367f2d518c40f64911872493dea51fe3066
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2021-04-27 08:54:05 +0200

    moved GUI out of game logic to its own module

---
+++ gymSnake/GUI.py
@@ -0,0 +1,50 @@
+# The purpose of this module is to provide the human interface for the snake game.
+import pygame
+import Config
+
+
+class Draw(object):
+    def __init__(self, grid):
+        self.screen_size = (Config.window_width, Config.window_height)
+        self.screen = pygame.display.set_mode(self.screen_size, 0, 32)
+        self.grid = grid
+        self.grid_color = ((93, 216, 228), (93, 150, 228))
+        self.font = pygame.font.SysFont("monospace", 32)
+        self.surface = pygame.Surface(self.screen.get_size()).convert()
+
+    def draw_grid(self):
+        for y in range(0, int(self.grid.height)):
+            for x in range(0, int(self.grid.width)):
+                color = self.grid_color[(x + y) % 2]  # alternating colors by (x + y) % 2
+                r = pygame.Rect((x * self.grid.size[0], y * self.grid.size[1]), self.grid.size)
+                pygame.draw.rect(self.surface, color, r)
+
+    def draw_food(self, food):
+        r = pygame.Rect(self.cal_position(food.position), self.grid.size)
+        pygame.draw.rect(self.surface, food.color, r)
+
+    def draw_snake(self, snake):
+        for p in snake.positions:
+            r = pygame.Rect(self.cal_position((p[0], p[1])), self.grid.size)
+            pygame.draw.rect(self.surface, snake.color, r)
+            pygame.draw.rect(self.surface, (93, 216, 228), r, 1)
+
+    def cal_position(self, position):
+        x, y = position
+        return x * self.grid.size[0], y * self.grid.size[1]
+
+    def draw_score(self, score):
+        score_text = self.font.render("Score {0}".format(score), True, (255, 0, 0))
+        self.screen.blit(score_text, (5, 10))
+
+    def draw_timer(self):
+        timer_text = self.font.render("Time {0}".format(int(pygame.time.get_ticks() / 1000)), True, (255, 0, 0))
+        self.screen.blit(timer_text, (5, 50))
+
+    def step(self, snake, food, score):
+        self.draw_grid()
+        self.draw_snake(snake)
+        self.draw_food(food)
+        self.screen.blit(self.surface, (0, 0))
+        self.draw_score(score)
+        self.draw_timer()
--- gymSnake/SnakeComponents.py
+++ gymSnake/SnakeComponents.py
@@ -1,61 +1,10 @@
+# The purpose of this module is to implement the game logic of the snake game
 import pygame
 import sys
 import Entity
 import Config
 import Utility
-
-
-SCREEN_WIDTH = 480
-SCREEN_HEIGHT = 480
-
-
-class Draw(object):
-    def __init__(self, screen_size, grid_width, grid_height, grid_size):
-        self.screen_size = screen_size
-        self.screen = pygame.display.set_mode(screen_size, 0, 32)
-        self.grid_width = grid_width
-        self.grid_height = grid_height
-        self.grid_size = grid_size
-        self.grid_color = ((93, 216, 228), (93, 150, 228))
-        self.font = pygame.font.SysFont("monospace", 32)
-        self.surface = pygame.Surface(self.screen.get_size()).convert()
-
-    def draw_grid(self):
-        for y in range(0, int(self.grid_height)):
-            for x in range(0, int(self.grid_width)):
-                color = self.grid_color[(x + y) % 2]  # alternating colors by (x + y) % 2
-                r = pygame.Rect((x * self.grid_size[0], y * self.grid_size[1]), self.grid_size)
-                pygame.draw.rect(self.surface, color, r)
-
-    def draw_food(self, food):
-        r = pygame.Rect(self.cal_position(food.position), self.grid_size)
-        pygame.draw.rect(self.surface, food.color, r)
-
-    def draw_snake(self, snake):
-        for p in snake.positions:
-            r = pygame.Rect(self.cal_position((p[0], p[1])), self.grid_size)
-            pygame.draw.rect(self.surface, snake.color, r)
-            pygame.draw.rect(self.surface, (93, 216, 228), r, 1)
-
-    def cal_position(self, position):
-        x, y = position
-        return x * self.grid_size[0], y * self.grid_size[1]
-
-    def draw_score(self, score):
-        score_text = self.font.render("Score {0}".format(score), True, (255, 0, 0))
-        self.screen.blit(score_text, (5, 10))
-
-    def draw_timer(self):
-        timer_text = self.font.render("Time {0}".format(int(pygame.time.get_ticks() / 1000)), True, (255, 0, 0))
-        self.screen.blit(timer_text, (5, 50))
-
-    def step(self, snake, food, score):
-        self.draw_grid()
-        self.draw_snake(snake)
-        self.draw_food(food)
-        self.screen.blit(self.surface, (0, 0))
-        self.draw_score(score)
-        self.draw_timer()
+import GUI
 
 
 class Game(object):
@@ -94,7 +43,7 @@ class Game(object):
         draw = None
         if mode == "human":
             if draw is None:
-                draw = Draw((SCREEN_WIDTH, SCREEN_HEIGHT), self.grid.width, self.grid.height, self.grid.size)
+                draw = GUI.Draw(self.grid)
             draw.step(self.snake, self.food, self.score)
             pygame.display.update()
 
