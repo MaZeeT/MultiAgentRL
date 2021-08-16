d8a5182d1ca1d207e9d4896830a18327e4219f50
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2021-08-16 13:02:15 +0200

    Update main.py

--- multi_control/multi_control/envs/main.py
+++ multi_control/multi_control/envs/main.py
@@ -1,30 +1,31 @@
 # Purpose of this file is to test run the environment with human control.
 # Should not be packaged into the Gym-package since a reinforcement algorithm doesn't have any use for it.
-from multi_control.envs import case
-from multi_control.envs import gym_environment
+from multi_control.envs import case, gym_environment
 from multi_control.envs.human_input import get_direction
 
-env = gym_environment.GymEnvironment()
-state = env.reset()
-agents = env.agents
+if __name__ == "__main__":
+    env = gym_environment.GymEnvironment()
+    state = env.reset()
+    agents = env.agents
 
-print("agent: " + str(agents))
-print("Entity_set: \n")
-env.render()
-
-counter = 0
-isRunning = True
-total_reward = 0
-while isRunning:
-    direc = []
-    for agent in agents:
-        direc.append(get_direction())
-    observation, reward, done, info = env.step(direc)
-    total_reward += reward
-    counter += 1
-    print("Moves taken:" + str(counter))
+    print("Entity_set:")
     env.render()
-    if done:
-        isRunning = False
-        print("finished")
-        print("reward is :{}".format(total_reward))
+
+    move_counter = 0
+    isRunning = True
+    total_reward = 0
+    while isRunning:
+        actions = []
+        for agent in agents:
+            actions.append(get_direction())
+
+        observation, reward, done, info = env.step(actions)
+        total_reward += reward
+        move_counter += 1
+        print("Moves taken:" + str(move_counter))
+        env.render()
+
+        if done:
+            isRunning = False
+            print("finished")
+            print("reward is :{}".format(total_reward))
