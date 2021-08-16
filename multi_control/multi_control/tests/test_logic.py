e89984c68f4f95a1e034cd828eeb2509f56fc38f
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2021-08-16 12:53:07 +0200

    Create test_logic.py

---
+++ multi_control/multi_control/tests/test_logic.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+from multi_control.envs import entities, gym_environment
+
+
+class TestObjectLogic(TestCase):
+
+    def test_move_agent_not_occupied(self):
+        entity_set = entities.EntitySet([
+            entities.Wall(0, 0),
+            entities.Wall(0, 2),
+        ])
+        agent = entities.Agent(0, 1)
+        entity_set.append(agent)
+        result = gym_environment.move_agent_in_list(entity_set, agent, "left")
+        self.assertTrue(result)
+        self.assertEqual(agent.x, -1)
+        self.assertEqual(agent.y, 1)
+
+    def test_move_agent_occupied(self):
+        entity_set = entities.EntitySet([
+            entities.Wall(0, 0),
+            entities.Wall(0, 2),
+        ])
+        agent = entities.Agent(0, 1)
+        entity_set.append(agent)
+        result = gym_environment.move_agent_in_list(entity_set, agent, "down")
+        self.assertFalse(result)
+        self.assertEqual(agent.x, 0)
+        self.assertEqual(agent.y, 1)
