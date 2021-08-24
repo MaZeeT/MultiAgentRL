44bd9a957b8479f1223d8c27b418f8335813a992
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2021-08-25 01:15:03 +0200

    Update gym_linear_path.py

--- multi_control/multi_control/envs/gym_linear_path.py
+++ multi_control/multi_control/envs/gym_linear_path.py
@@ -22,13 +22,14 @@ class GymLinearPath(BaseEnv):
 
     def get_field(self):
         agents = [entities.Agent(3, 8, group_id=1, id=3)]
-        agents += [entities.Agent(2, 2, group_id=2, id=2), entities.Agent(3, 4, group_id=2, id=2), entities.Agent(4, 2, group_id=2, id=2)]
+        agents += [entities.Agent(2, 2, group_id=2, id=2), entities.Agent(3, 4, group_id=2, id=2),
+                   entities.Agent(4, 2, group_id=2, id=2)]
 
         goals = [entities.Goal(9, 7, interactive_with_group_id=1), entities.Goal(10, 14, interactive_with_group_id=1)]
 
         walls = []
-        for i in range(1,11):
-            walls.append(entities.Wall(6,i))
+        for i in range(1, 11):
+            walls.append(entities.Wall(6, i))
 
         removableWallsPositions = [(1, 6), (2, 6), (3, 6), (4, 6), (5, 6)]
         rWalls = entities.ParentRemovableWall(removableWallsPositions, interactive_with_group_id=1).children
@@ -37,18 +38,18 @@ class GymLinearPath(BaseEnv):
         rWalls += entities.ParentRemovableWall(removableWallsPositionsTwo, interactive_with_group_id=1).children
 
         explodingWallPositions = []
-        for i in range(1,6):
-            explodingWallPositions.append((i,10))
+        for i in range(1, 6):
+            explodingWallPositions.append((i, 10))
         eWalls = entities.ExplodingWall(explodingWallPositions, interactive_with_group_id=2).children
 
         explodingWallPositions = []
-        for i in range(7,12):
-            explodingWallPositions.append((i,10))
+        for i in range(7, 12):
+            explodingWallPositions.append((i, 10))
         eWalls += entities.ExplodingWall(explodingWallPositions, interactive_with_group_id=2).children
 
         explodingWallPositions = []
-        for i in range(7,12):
-            explodingWallPositions.append((i,4))
+        for i in range(7, 12):
+            explodingWallPositions.append((i, 4))
         eWalls += entities.ExplodingWall(explodingWallPositions, interactive_with_group_id=2).children
 
         set = self.add_outer_walls(x=12, y=16)
