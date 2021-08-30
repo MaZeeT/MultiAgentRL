c6438854abfe9b8d3be9c51c8a00dab787b8a8b3
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2021-08-30 21:01:22 +0200

    Update env_basic_cooperation.py

--- multi_agent_environments/multi_agent_environments/envs/env_basic_cooperation.py
+++ multi_agent_environments/multi_agent_environments/envs/env_basic_cooperation.py
@@ -11,17 +11,19 @@ class BasicCooperation(BaseEnv):
         super().__init__()
 
     def get_field(self):
-        agents = [entities.Agent(3, 3, group_id=1, id=3), entities.Agent(3, 9, group_id=2, id=2)]
-        goals = [entities.Goal(2, 15, interactive_with_group_id=1), entities.Goal(4, 15, interactive_with_group_id=2)]
+        agents, goals, rWalls = [], [], []
+
+        agents += [entities.Agent(3, 3, group_id=1, id=3), entities.Agent(3, 9, group_id=2, id=2)]
+        goals += [entities.Goal(2, 15, interactive_with_group_id=1), entities.Goal(4, 15, interactive_with_group_id=2)]
 
         removableWallsPositions = [(1, 6), (2, 6), (3, 6), (4, 6), (5, 6)]
-        rWalls = entities.ParentRemovableWall(removableWallsPositions, interactive_with_group_id=2)
+        rWalls += entities.ParentRemovableWall(removableWallsPositions, interactive_with_group_id=2).children
 
-        interactiveWallPositions = [(1, 12), (2, 12), (3, 12), (4, 12), (5, 12)]
-        iWalls = entities.ParentRemovableWall(interactiveWallPositions, interactive_with_group_id=2)
+        removableWallsPositions = [(1, 12), (2, 12), (3, 12), (4, 12), (5, 12)]
+        rWalls += entities.ParentRemovableWall(removableWallsPositions, interactive_with_group_id=2).children
 
         set = self.add_outer_walls(x=6, y=18)
-        set += agents + goals + rWalls.children + iWalls.children
+        set += agents + goals + rWalls
 
         entity_set = entities.EntitySet(set)
         return agents, entity_set
